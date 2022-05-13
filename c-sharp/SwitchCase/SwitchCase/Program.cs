331e7d1f74f9fdc9f8f9996734339a8f81cd5523
Roberto Mac lean <roberto.maclean@bicevida.cl>
2022-05-12 22:38:18 -0400

    feat: switch case class 7

---
+++ c-sharp/SwitchCase/SwitchCase/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SwitchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int op = 1;
+
+            switch (op)
+            {
+                case 1:
+                    Console.WriteLine("Caso 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Case 2");
+                    break;
+                case 3:
+                case 4:
+                    Console.WriteLine("Seleccionaste el caso 3 o 4");
+                    break;
+                default:
+                    Console.WriteLine("Invalido");
+                    break;
+            }
+        }
+    }
+}
