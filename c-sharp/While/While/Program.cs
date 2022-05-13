76197bcb5dd3d51293970beacbf277e006a16a1d
Roberto Mac lean <roberto.maclean@bicevida.cl>
2022-05-12 23:15:48 -0400

    feat: arreglos class 9

---
+++ c-sharp/While/While/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while(i < 10)
+            {
+                Console.WriteLine("Iteración de i " + i);
+                i++;
+            }
+            int j = 0;
+            while(j < 100)
+            {
+                if(j > 10) 
+                    break;
+                Console.WriteLine("iteración de j " + j);
+                j++;
+            }
+        }
+    }
+}
