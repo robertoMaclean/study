5e9214a6feeb176ba741d3ee43463f213bc42c53
Roberto Mac lean <roberto.maclean@bicevida.cl>
2022-05-13 16:10:26 -0400

    feat: class and object class 11

---
+++ c-sharp/ClasesYObjetos/ClasesYObjetos/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ClasesYObjetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sale sale = new Sale(100, DateTime.Now);
+            string showInfo = sale.ShowInfo();
+            Console.WriteLine(showInfo);
+        }
+    }
+
+    class Sale
+    {
+        int total;
+        DateTime date;
+
+        public Sale(int total, DateTime date)
+        {
+            this.total = total;
+            this.date = date;
+        }
+
+        public string ShowInfo()
+        {
+            return this.total + " " + this.date.ToLongDateString();
+        }
+        public void Show()
+        {
+            Console.WriteLine("Hola soy una venta!");
+        }
+    }
+}
