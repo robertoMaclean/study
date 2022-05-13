a11f54fb6647fca56feb6be0a92c571a3a7d750f
Roberto Mac lean <roberto.maclean@bicevida.cl>
2022-05-12 23:25:20 -0400

    feat: for class 10

---
+++ c-sharp/For/For/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] friends = new string[6]
+            {
+                "juan",
+                "pedro",
+                "carlos",
+                "sebastian",
+                "cristian",
+                "camilo"
+            };
+            for(int i = 0; i < friends.Length; i++)
+            {
+                Console.WriteLine(friends[i]);
+            }
+        }
+    }
+}
