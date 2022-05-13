692b7335270625da92b49c6c033912880fc8b4b7
Roberto Mac lean <roberto.maclean@bicevida.cl>
2022-05-12 23:06:04 -0400

    array class 8

---
+++ c-sharp/Arreglos/Arreglos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Arreglos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] friends = new string[7]{
+                "juan",
+                "pedro",
+                "diago",
+                "marcelo",
+                null,
+                "carlos",
+                null,
+            };
+            for(int i = 0; i < friends.Length; i ++)
+            {
+                Console.WriteLine(friends[i]);
+            }
+        }
+    }
+}
