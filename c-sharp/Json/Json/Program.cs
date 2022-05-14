437c961774117020c80e3a87f1627c80b190db01
Roberto Mac lean <roberto.maclean@bicevida.cl>
2022-05-14 08:39:56 -0400

    feat: json class 19-20

---
+++ c-sharp/Json/Json/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text.Json;
+namespace Json
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Beer myBeer = new Beer()
+            {
+                Name = "Baltica",
+                Brand = "Erdinger"
+            };
+
+            string json = JsonSerializer.Serialize(myBeer);
+            Beer beer = JsonSerializer.Deserialize<Beer>(json);
+            Console.WriteLine(json);
+
+            Beer[] myBeers = new Beer[]
+            {
+                new Beer()
+                {
+                    Name = "Baltica",
+                    Brand = "Erdinger"
+                 },
+                new Beer()
+                {
+                    Name = "Cristan",
+                    Brand = "Modelo"
+                }
+            };
+            string jsonBeers = JsonSerializer.Serialize(myBeers);
+            Beer[] beers = JsonSerializer.Deserialize<Beer[]>(jsonBeers);
+            Console.WriteLine(jsonBeers);
+
+        }
+    }
+
+    public class Beer
+    {
+        public string Name { get; set; }
+        public string Brand { get; set; }
+    }
+}
