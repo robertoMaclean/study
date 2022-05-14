bf1f91fd4bae51df6a66f74d7737fdb36ae43c3e
Roberto Mac lean <roberto.maclean@bicevida.cl>
2022-05-14 06:23:57 -0400

    feat: static class 16

---
+++ c-sharp/Static/Static/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Static
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            People people1 = new People()
+            {
+                Name = "Rob",
+                Age = 50
+            };
+            Console.WriteLine(People.Count);
+            People people2 = new People()
+            {
+                Name = "Pedrito jr.",
+                Age = 15
+            };
+            Console.WriteLine(People.Count);
+            Console.WriteLine(People.GetCount());
+        }
+    }
+
+    public class People
+    { 
+        public static int Count = 0;
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public People()
+        {
+            Count++;
+        }
+
+        public static string GetCount()
+        {
+            return $"Esta clase se ha utilizado {Count} veces"; 
+        }
+
+
+    }
+}
