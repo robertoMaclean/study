7d8e72cbce8cb4ffa0bfc126515c6110496d487c
Roberto Mac lean <roberto.maclean@bicevida.cl>
2022-05-14 13:39:54 -0400

    feat: list common methos class 23

---
+++ c-sharp/ForEach/ForEach/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ForEach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = 5;
+            var numbers = new List<int>()
+            {
+                23,2,12,3,123,124
+            };
+
+            foreach(var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            var students = new List<People>()
+            {
+                new People() { Name = "Héctor", Country = "México" },
+                new People() { Name = "Rob", Country = "Bolivia" },
+                new People() { Name = "John", Country = "USA" }
+            };
+            Show(students);
+            students.RemoveAt(0);
+            Show(students);
+        }
+
+        static void Show(List<People> students)
+        {
+            Console.WriteLine("-- Personas --");
+            foreach(var people in students)
+            {
+                Console.WriteLine($"Nombre: {people.Name}, País: {people.Country}");
+            }
+        }
+    }
+
+    class People
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+    }
+}
---
+++ c-sharp/ListCommonMethods/ListCommonMethods/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListCommonMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>()
+            {
+                4,3,5,19
+            };
+            Show(numbers);
+            numbers.Insert(1, 666);
+            Show(numbers);
+            if (numbers.Contains(666))
+                Console.WriteLine("Bienvenido Satanás!!!");
+            else
+                Console.WriteLine("Hola canuto");
+            var index = numbers.IndexOf(666);
+            Console.WriteLine(index);
+            Console.WriteLine("-- IndexOf ---");
+            index = numbers.IndexOf(100);
+            Console.WriteLine(index);
+            numbers.Sort();
+            Console.WriteLine("-- Sort ---");
+            Show(numbers);
+            var numbers2 = new List<int>()
+            {
+                200, 300, 500
+            };
+            numbers.AddRange(numbers2);
+            Show(numbers);
+        }
+
+        public static void Show(List<int> numbers)
+        {
+            Console.WriteLine("-- Numeros --");
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
