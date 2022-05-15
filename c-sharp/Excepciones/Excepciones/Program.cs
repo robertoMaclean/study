3b881b44c57d1a4e4c8962e48da4d28c0c939442
Roberto Mac lean <roberto.maclean@bicevida.cl>
2022-05-14 21:46:26 -0400

    feat: exception class 26

---
+++ c-sharp/Excepciones/Excepciones/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Excepciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var content = File.ReadAllText("/Users/robertomclean/clientes.txt");
+                Console.WriteLine(content);
+
+                // var content2 = File.ReadAllText("/Users/robertomclean/clientes2.txt");
+                // Console.WriteLine(content2);
+                throw new Exception("Apareció Doom Guy");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("El archivo no existe");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+           
+        }
+    }
+}
