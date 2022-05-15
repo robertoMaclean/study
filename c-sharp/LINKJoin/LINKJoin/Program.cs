57746f58eb55f375b61ddba15c11bb6b41df8b61
Roberto Mac lean <roberto.maclean@bicevida.cl>
2022-05-14 23:25:30 -0400

    feat: LINQJoin class 29

---
+++ c-sharp/LINKJoin/LINKJoin/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINKJoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var beers = new List<Beer>() {
+                new Beer()
+                {
+                    Name = "Corona", Country = "México"
+                },
+                new Beer()
+                {
+                    Name = "Delirium", Country = "Bélgica"
+                },
+                new Beer()
+                {
+                    Name = "Erdinger", Country = "Alemania"
+                }
+            };
+            var countries = new List<Country>()
+            {
+                new Country()
+                {
+                    Name = "México", Continent = "América"
+                },
+                new Country()
+                {
+                    Name = "Bélgica", Continent = "Europa"
+                },
+                new Country()
+                {
+                    Name = "Alemania", Continent = "Europa"
+                }
+            };
+            var beersWithContinent = from beer in beers
+                                     join country in countries
+                                     on beer.Country equals country.Name
+                                     select new
+                                     {
+                                         Name = beer.Name,
+                                         Country = beer.Country,
+                                         Continent = country.Continent
+                                     };
+            foreach(var beer in beersWithContinent)
+            {
+                Console.WriteLine($"{beer.Name} {beer.Country} {beer.Continent}");
+            }
+                                   
+        }
+    }
+
+    class Beer
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+    }
+
+    class Country
+    {
+        public string Name { get; set; }
+        public string Continent { get; set; }
+    }
+}
