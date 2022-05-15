58d58cb3969fdef31183a729c4e3af0e223aa1ac
Roberto Mac lean <roberto.maclean@bicevida.cl>
2022-05-14 21:26:39 -0400

    feat: tuples class 25

---
+++ c-sharp/TipoAnonimos/TipoAnonimos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TipoAnonimos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rob = new
+            {
+                Name = "Rob",
+                Country = "Italy"
+            };
+            Console.WriteLine($"{rob.Name} {rob.Country}");
+
+            var beers = new[]
+            {
+                new { Name = "Red", Brand = "Delirium"},
+                new { Name = "Lodon Porter", Brand = "Fullers"}
+            };
+            foreach(var beer in beers)
+            {
+                Console.WriteLine($"Cerveza {beer.Name} {beer.Brand}");
+            }
+        }
+    }
+}
---
+++ c-sharp/Tuplas/Tuplas/Program.cs
@@ -0,0 +1,52 @@
+﻿ using System;
+
+namespace Tuplas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (int id, string name) product = (1, "Cerveza Stout");
+            Console.WriteLine($"{product.id} {product.name}");
+            product.name = "Cerveza Porter";
+            Console.WriteLine($"{product.id} {product.name}");
+            var person = (1, "Juan");
+            Console.WriteLine($"Person {person.Item1} {person.Item2}");
+            var people = new[]
+            {
+                (1, "Héctor"),
+                (2, "Juan"),
+                (3, "Marcelo"),
+                (4, "Paul")
+            };
+            Console.WriteLine("-- People --");
+            foreach(var p in people)
+            {
+                Console.WriteLine($"{p.Item1} {p.Item2}");
+            }
+            (int id, string name)[] people2 = new[]
+            {
+                (1, "Héctor"),
+                (2, "Juan"),
+                (3, "Marcelo"),
+                (4, "Paul")
+            };
+            foreach (var p in people2)
+            {
+                Console.WriteLine($"{p.id} {p.name}");
+            }
+            var cityInfo = getLocationCDMX();
+            Console.WriteLine($"lat: {cityInfo.lat}, lng: {cityInfo.lng}, name: {cityInfo.name}");
+            var (_, lng, _) = getLocationCDMX();
+            Console.WriteLine(lng);
+        }
+
+        public static (float lat, float lng, string name) getLocationCDMX()
+        {
+            float lat = 19.12121f;
+            float lng = -99.12123f;
+            string name = "CDMX";
+            return (lat, lng, name); 
+        }
+    }
+}
