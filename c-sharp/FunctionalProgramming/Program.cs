9851abbcec21c783caa169280494487251d47d25
Roberto Mac lean <roberto.maclean@bicevida.cl>
2022-05-27 12:01:46 -0400

    delegates class 42

---
+++ c-sharp/FunctionalProgramming/Program.cs
@@ -0,0 +1,18 @@
+﻿Operation mySum = Functions.Sum;
+mySum = Functions.Mul;
+Console.WriteLine(mySum(2, 3));
+
+Show show = Functions.ConsoleShow;
+show("hola mundo");
+
+delegate int Operation(int a, int b);
+delegate void Show(string message);
+
+
+public class Functions
+{
+    public static int Sum(int x, int y) => x + y;
+    public static int Mul(int x, int y) => x * y;
+    public static void ConsoleShow(string m) => Console.WriteLine(m);
+
+}
