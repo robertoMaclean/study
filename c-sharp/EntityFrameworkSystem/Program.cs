456e302b02830d4cd572dfd537f33e9ccd861c4b
Roberto Mac lean <roberto.maclean@bicevida.cl>
2022-05-27 10:04:22 -0400

    feat: Entity Framework get all class 38

--- c-sharp/EntityFrameworkSystem/Program.cs
+++ c-sharp/EntityFrameworkSystem/Program.cs
@@ -1,6 +1,5 @@
 ﻿using BD;
 using Microsoft.EntityFrameworkCore;
-using System.Linq;
 
 namespace EntityFrameworkSystem
 {
@@ -10,15 +9,60 @@ namespace EntityFrameworkSystem
         {
             DbContextOptionsBuilder<CsharpDBContext> optionBuilder = new DbContextOptionsBuilder<CsharpDBContext>();
             optionBuilder.UseSqlServer(@"Server=ROB-WIN-PC\SQLEXPRESS;Database=CsharpDB;Trusted_Connection=True;");
-            using (CsharpDBContext context = new CsharpDBContext(optionBuilder.Options))
-            {
-                var beers = context.Beers.ToList();
 
-                foreach (var beer in beers)
+            bool again = true;
+            int op = 0;
+
+            do
+            {
+                ShowMenu();
+                Console.WriteLine("Elige una opción:");
+                op = int.Parse(Console.ReadLine());
+                switch (op)
                 {
-                    Console.WriteLine(beer.Name);
+                    case 1:
+                        Show(optionBuilder);
+                        break;
+                }
+            } while (again);
+        }
+
+        public static void ShowMenu()
+        {
+            Console.WriteLine("\n---------------Menu------------------");
+            Console.WriteLine("1.- Mostrar");
+            Console.WriteLine("2.- Agregar");
+            Console.WriteLine("3.- Editar");
+            Console.WriteLine("4.- Eliminar");
+            Console.WriteLine("5.- Salir");
+        }
+        public static void Show(DbContextOptionsBuilder<CsharpDBContext> optionBuilder)
+        {
+            Console.Clear();
+            Console.WriteLine("Cervezas en la base de datos");
+            using (var context = new CsharpDBContext(optionBuilder.Options))
+            {
+                List<Beer> beers = context.Beers.OrderBy(beer => beer.Name).ToList();
+                foreach(var beer in beers)
+                {
+                    Console.WriteLine($"{beer.Id} - {beer.Name}");
                 }
             }
+        }
+
+        public static void Add()
+        {
+           
+        }
+
+        public static void Edit()
+        {
+           
+        }
+
+        public static void Delete()
+        {
+
         }
     }
 }
