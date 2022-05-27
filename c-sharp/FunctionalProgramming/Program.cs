ac7226fa07267134bcdc2b64685f5ab3f1b4dd36
Roberto Mac lean <roberto.maclean@bicevida.cl>
2022-05-27 12:57:54 -0400

    feat: composite functions class 43

--- c-sharp/FunctionalProgramming/Program.cs
+++ c-sharp/FunctionalProgramming/Program.cs
@@ -2,17 +2,26 @@
 mySum = Functions.Mul;
 Console.WriteLine(mySum(2, 3));
 
-Show show = Functions.ConsoleShow;
-show("hola mundo");
+Show cw = Console.WriteLine;
+cw += Functions.ConsoleShow;
 
+Functions.Some("Juan", "Carlitos", cw);
 delegate int Operation(int a, int b);
-delegate void Show(string message);
+public delegate void Show(string message);
 
 
 public class Functions
 {
     public static int Sum(int x, int y) => x + y;
     public static int Mul(int x, int y) => x * y;
-    public static void ConsoleShow(string m) => Console.WriteLine(m);
+    public static void ConsoleShow(string m) => Console.WriteLine(m.ToUpper());
+
+    public static void Some(string name, string lastName, Show fn)
+    {
+        Console.WriteLine("Hago algo al inicio");
+        fn($"Hola {name} {lastName}");
+        Console.WriteLine("Hago algo al final");
+
+    }
 
 }
