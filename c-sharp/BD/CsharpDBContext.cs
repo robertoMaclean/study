f2dd83275b89a28e774029a528c224bb9f07c378
Roberto Mac lean <roberto.maclean@bicevida.cl>
2022-05-27 09:44:26 -0400

    feat: Entity Framework class 36-37

---
+++ c-sharp/BD/CsharpDBContext.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace BD
+{
+    public partial class CsharpDBContext : DbContext
+    {
+        public CsharpDBContext()
+        {
+        }
+
+        public CsharpDBContext(DbContextOptions<CsharpDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Beer> Beers { get; set; } = null!;
+        public virtual DbSet<Brand> Brands { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=ROB-WIN-PC\\SQLEXPRESS; Database=CsharpDB; Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Beer>(entity =>
+            {
+                entity.ToTable("Beer");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.BrandId).HasColumnName("BrandID");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsFixedLength();
+
+                entity.HasOne(d => d.Brand)
+                    .WithMany(p => p.Beers)
+                    .HasForeignKey(d => d.BrandId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Beer_Brand");
+            });
+
+            modelBuilder.Entity<Brand>(entity =>
+            {
+                entity.ToTable("Brand");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsFixedLength();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ c-sharp/EntityFrameworkSystem/Program.cs
@@ -0,0 +1,24 @@
+﻿using BD;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace EntityFrameworkSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DbContextOptionsBuilder<CsharpDBContext> optionBuilder = new DbContextOptionsBuilder<CsharpDBContext>();
+            optionBuilder.UseSqlServer(@"Server=ROB-WIN-PC\SQLEXPRESS;Database=CsharpDB;Trusted_Connection=True;");
+            using (CsharpDBContext context = new CsharpDBContext(optionBuilder.Options))
+            {
+                var beers = context.Beers.ToList();
+
+                foreach (var beer in beers)
+                {
+                    Console.WriteLine(beer.Name);
+                }
+            }
+        }
+    }
+}
