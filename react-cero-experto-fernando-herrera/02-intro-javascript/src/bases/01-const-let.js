e3a78c744086eccd60f2232f0f62f938dd56cd3b
Roberto Mac lean <roberto.maclean@bicevida.cl>
2022-11-04 13:01:15 -0300

    feat: add react class 14 let and const

---
+++ react-cero-experto-fernando-herrera/02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,12 @@
+
+const nombre = 'Rob';
+const apellido = 'Mac lean';
+let valorDado = 5;
+valorDado = 4;
+console.log(nombre, apellido, valorDado)
+if(true) {
+  const nombre = 'Rob';
+  console.log(nombre);
+}
+
+console.log(valorDado);
