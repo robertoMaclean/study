0ae47a460414273d174acc4b1952a0e667116fc4
Roberto Mac lean <roberto.maclean@bicevida.cl>
2022-11-06 09:52:22 -0300

    feat: functions class 18

---
+++ react-cero-experto-fernando-herrera/02-intro-javascript/src/bases/05-funciones.js
@@ -0,0 +1,48 @@
+
+// Funciones en JS
+const saludar = function saludar( nombre ) {
+	return `Hola, ${ nombre }`;
+}
+
+const saludar2 = ( nombre ) => {
+	return `Hola, ${ nombre }`;
+}
+
+const saludar3 = ( nombre ) => `Hola, ${ nombre }`;
+
+const saludar4 = () => `Hola Mundo`;
+
+console.log( saludar('Goku') )
+console.log( saludar2('Vegeta') )
+console.log( saludar3('Goku') )
+console.log( saludar4() )
+
+const getUser = () => ({
+	uid: 'ABC123',
+	username: 'El Papi1502'
+}) 
+
+console.log( getUser() )
+
+// Tarea
+// 1. Transformar a función de Flecha
+// 2. Tiene que retornar un objeto implícito
+// 3. Pruebas
+function getUsuarioActivo( nombre ) {
+	return {
+		uid: 'ABC567',
+		username: nombre
+	}
+}
+
+const getUsuarioActivo2 = ( nombre ) => ({
+	uid: 'ABC567',
+	username: nombre
+})
+
+
+const usuarioActivo = getUsuarioActivo( 'Roberto' );
+console.log( usuarioActivo )
+
+const usuarioActivo2 = getUsuarioActivo2( 'RobReznor' )
+console.log( usuarioActivo2 )
