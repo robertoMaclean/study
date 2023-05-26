5d9cf142d63150aabd38d670c02ab0c1768e49b2
Roberto Mac lean <roberto.maclean@bicevida.cl>
2023-05-26 11:29:50 -0400

    feat: add class 45 useState hook

--- react-cero-experto-fernando-herrera/03-counter-app-vite/src/CounterApp.jsx
+++ react-cero-experto-fernando-herrera/03-counter-app-vite/src/CounterApp.jsx
@@ -1,17 +1,16 @@
+import { useState } from 'react'
 import PropTypes from 'prop-types'
 
 export const CounterApp = ({ value }) => {
-
+	const [ counter, setCounter ] = useState( value )
 	const handleAdd = () => {
-		console.log( '+1' )
-		value = 1000
-		console.log( value )
+		setCounter( counter + 1 )
 	}
 
 	return (
 	<>
 		<h1>CounterApp</h1>
-		<h2> { value } </h2>
+		<h2> { counter } </h2>
 		<button onClick={ handleAdd }>
 			+ 1
 		</button>
