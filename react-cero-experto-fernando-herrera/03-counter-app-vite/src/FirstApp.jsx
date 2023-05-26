b97c5dfb27d959150c09e0db5f8dced4b237b99a
Roberto Mac lean <roberto.maclean@bicevida.cl>
2023-05-25 22:07:09 -0400

    feat: add class 38 vars render

--- react-cero-experto-fernando-herrera/03-counter-app-vite/src/FirstApp.jsx
+++ react-cero-experto-fernando-herrera/03-counter-app-vite/src/FirstApp.jsx
@@ -1,8 +1,16 @@
 
+const newMessage = {
+	message: 'Hola Mundo',
+	titulo: 'Rob'
+}
+
+const getMessage = (name) => `Hello ${name}`
+
 export const FirstApp = () => {
 	return (
 		<>
-			<h1>RobReznor</h1>
+			<h1>{ getMessage('Rob') }</h1>
+			{/*<code>{ JSON.stringify( newMessage ) }</code>*/}
 			<p>Soy un subtitulo</p>
 		</>
 	)
