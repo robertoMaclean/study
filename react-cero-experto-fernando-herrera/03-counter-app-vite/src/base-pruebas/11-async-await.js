77bebfd273bb568e695c8b2a68cb6bf6d910481a
Roberto Mac lean <roberto.maclean@bicevida.cl>
2023-05-29 16:18:21 -0400

    feat: add class 61 evaluate catch in async await

--- react-cero-experto-fernando-herrera/03-counter-app-vite/src/base-pruebas/11-async-await.js
+++ react-cero-experto-fernando-herrera/03-counter-app-vite/src/base-pruebas/11-async-await.js
@@ -5,17 +5,18 @@ export const getImagen = async() => {
 
     try {
 
-        const apiKey = 'z3Yz571Mw1AtJg8xnQMBU4XGk8NGJFdN';
+        //const apiKey = 'z3Yz571Mw1AtJg8xnQMBU4XGk8NGJFdN';
+        const apiKey = ''
         const resp   = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`);
         const { data } = await resp.json(); 
 
         const { url } = data.images.original;
-
+        console.log( url )
         return url
 
     } catch (error) {
         // manejo del error
-        console.error(error)
+        // console.error(error)
         return 'No se encontró la imagen'
     }
     
--- react-cero-experto-fernando-herrera/03-counter-app-vite/tests/base-pruebas/11-async-await.test.js
+++ react-cero-experto-fernando-herrera/03-counter-app-vite/tests/base-pruebas/11-async-await.test.js
@@ -1,9 +1,9 @@
 import {getImagen} from "../../src/base-pruebas/11-async-await"
 
 describe('Pruebas en 11-async-await.js', () => {
-  test('getImagen debe de retornar un URL de la imagen', async() => {
+  test('getImagen debe de retornar un error si no tenemos la API KEY', async() => {
       
     const url = await getImagen()
-    expect( typeof url ).toBe( 'string' )
+    expect( url ).toBe( 'No se encontró la imagen' )
   })
 })
