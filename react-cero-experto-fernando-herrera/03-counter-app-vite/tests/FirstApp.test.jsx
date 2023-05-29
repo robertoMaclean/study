fe5bf264bc480e345996f49b378056aa9b255c9c
Roberto Mac lean <roberto.maclean@bicevida.cl>
2023-05-29 17:33:05 -0400

    feat: add class 64 test FirsApp

--- react-cero-experto-fernando-herrera/03-counter-app-vite/tests/FirstApp.test.jsx
+++ react-cero-experto-fernando-herrera/03-counter-app-vite/tests/FirstApp.test.jsx
@@ -4,7 +4,19 @@ import { render } from '@testing-library/react'
 describe('Pruebas en <FirstApp />', () => {
 
 	test('debe de hacer match con el snapshot', () => {
-		render( <FirstApp /> )	
+		const title = 'Hola, Soy Goku'
+		const { container } = render( <FirstApp title = { title } /> )	
+
+		expect( container ).toMatchSnapshot()
+	})
+
+	test('debe mostrar el título en h1', () => {
+		const title = 'Hola, Soy Goku'
+		const { container, getByText } = render( <FirstApp title= { title } />) 
+		expect( getByText( title ) ).toBeTruthy()
+
+		const h1 = container.querySelector('h1')
+		expect( h1.innerHTML ).toBe( title )
 	})
 
 })
