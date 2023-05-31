2ab50e6caf640bcaa7e7fbafef5f77f81d23222e
Roberto Mac lean <roberto.maclean@bicevida.cl>
2023-05-30 21:58:24 -0400

    feat: add class 68 simulate click event

--- react-cero-experto-fernando-herrera/03-counter-app-vite/src/CounterApp.jsx
+++ react-cero-experto-fernando-herrera/03-counter-app-vite/src/CounterApp.jsx
@@ -19,10 +19,10 @@ export const CounterApp = ({ value }) => {
   return (
     <>
       <h1>CounterApp</h1>
-      <h2 data-testid = "test-counter"> { counter } </h2>
-      <button onClick={ handleAdd }> + 1 </button>
-      <button onClick={ handleSubstract }> -1 </button>
-      <button onClick={ handleReset }> Reset </button>
+      <h2 data-testid = "test-counter">{ counter }</h2>
+      <button onClick={ handleAdd }>+1</button>
+      <button onClick={ handleSubstract }>-1</button>
+      <button aria-label="btn-reset" onClick={ handleReset }>Reset</button>
     </>
   )
 }
--- react-cero-experto-fernando-herrera/03-counter-app-vite/tests/CounterApp.test.jsx
+++ react-cero-experto-fernando-herrera/03-counter-app-vite/tests/CounterApp.test.jsx
@@ -1,5 +1,5 @@
 import { CounterApp } from '../src/CounterApp'
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 
 describe('Pruebas en <CounterApp />', () => {
 	
@@ -16,7 +16,28 @@ describe('Pruebas en <CounterApp />', () => {
 		render(<CounterApp value = { 100 }/>)
 		expect( screen.getByText( 100 ) ).toBeTruthy()
 
-		expect( screen.getByRole('heading', { level: 2 }).innerHTML ).toContain( '100' )
+		//expect( screen.getByRole('heading', { level: 2 }).innerHTML ).toContain( '100' )
+
+	})
+
+	test('debe de incrementar con el botón +1', () => {
+
+		render( <CounterApp value = { initialValue } /> )
+		fireEvent.click( screen.getByText( '-1' ) )
+		expect( screen.getByText( '9' ) ).toBeTruthy()
+
+	})
+
+	test('debe de funcionar el botón de reset', () => {
+
+		render( <CounterApp value = { 355 } /> )
+		fireEvent.click( screen.getByText( '+1' ) )
+		fireEvent.click( screen.getByText( '+1' ) )
+		fireEvent.click( screen.getByText( '+1' ) )
+		//fireEvent.click( screen.getByText( 'Reset' ) )
+		fireEvent.click(screen.getByRole('button', { name: 'btn-reset' }))
+
+		expect( screen.getByText( 355 ) ).toBeTruthy()
 
 	})
 
