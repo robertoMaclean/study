0b89d8b5fa98ab51c088850086690ab9f87d540d
Roberto Mac lean <roberto.maclean@bicevida.cl>
2023-06-01 10:42:47 -0400

    feat: add class 75 category list

--- react-cero-experto-fernando-herrera/04-gif-expert-app/src/GifExpertApp.tsx
+++ react-cero-experto-fernando-herrera/04-gif-expert-app/src/GifExpertApp.tsx
@@ -1,7 +1,26 @@
+import { useState } from "react"
+
 export const GifExpertApp = () => {
+
+  const [ categories, setCategories ] = useState([ 'One Punch', 'Dragon Ball' ])
+  console.log( categories )
   return (
     <>
+      {/*titulo*/}
       <h1>GifExpertApp</h1>
+
+      {/*Input*/}
+
+      {/*Listado de Gif*/}
+      <ol>
+        {
+          categories.map( category => {
+            return <li key= { category }>{ category }</li>
+          })
+        }
+
+      </ol>
+        {/*Gif Item*/}
     </>
   )
 }
