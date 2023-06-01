4aa69da9e995cab37f651f5fe49a671f22447363
Roberto Mac lean <roberto.maclean@bicevida.cl>
2023-06-01 12:39:42 -0400

    feat: add class 78 components comunication

--- react-cero-experto-fernando-herrera/04-gif-expert-app/src/GifExpertApp.tsx
+++ react-cero-experto-fernando-herrera/04-gif-expert-app/src/GifExpertApp.tsx
@@ -1,16 +1,17 @@
 import { useState } from "react"
+import {AddCategory} from "./components/AddCategory"
 
 export const GifExpertApp = () => {
 
   const [ categories, setCategories ] = useState([ 'One Punch', 'Dragon Ball' ])
-  console.log( categories )
+  
   return (
     <>
       {/*titulo*/}
       <h1>GifExpertApp</h1>
 
       {/*Input*/}
-
+      <AddCategory setCategories={ setCategories }/> 
       {/*Listado de Gif*/}
       <ol>
         {
@@ -18,7 +19,6 @@ export const GifExpertApp = () => {
             return <li key= { category }>{ category }</li>
           })
         }
-
       </ol>
         {/*Gif Item*/}
     </>
---
+++ react-cero-experto-fernando-herrera/04-gif-expert-app/src/components/AddCategory.tsx
@@ -0,0 +1,29 @@
+import { FormEvent, useState} from "react"
+
+export const AddCategory = ( { setCategories } ) => {
+
+  const [ inputValue, setInputValue ] = useState('')
+
+  const onInputChange = ( event: any ) => {
+    setInputValue( event.target.value ) 
+  }
+
+  const onSubmit = ( event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    if( inputValue.trim().length < 1 ) return
+    setCategories( categories => [ inputValue, ...categories ] )
+    setInputValue('')
+  }
+
+  return (
+    
+    <form onSubmit={ (event) => onSubmit( event ) }>
+      <input
+        type = "text"
+        placeholder = "Buscar gifs"
+        value = { inputValue }
+        onChange = { onInputChange }
+      /> 
+    </form>
+  )
+}
