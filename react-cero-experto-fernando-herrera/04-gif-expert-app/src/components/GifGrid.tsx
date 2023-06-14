d351e20e7a4d164fd4cc175e6df2930995b7165b
Roberto Mac lean <roberto.maclean@bicevida.cl>
2023-06-13 22:23:18 -0400

    feat: add class 87 custom hooks

--- react-cero-experto-fernando-herrera/04-gif-expert-app/src/components/GifGrid.tsx
+++ react-cero-experto-fernando-herrera/04-gif-expert-app/src/components/GifGrid.tsx
@@ -1,24 +1,15 @@
-import { useEffect, useState } from 'react'
 import { GifItem } from './GifItem'
-import { getGifts } from '../helpers/getGifs'
+import { useFetchGifs } from '../hooks/useFetchGifs'
 
 export const GifGrid = ({ category }: any) => {
-  const [images, setImages] = useState([])
-  const getImages = async () => {
-    const newImages = await getGifts(category)
-    setImages(newImages)
-  }
-  useEffect(() => {
-    getImages()
-  }, [])
-
+  const { images, isLoading } = useFetchGifs(category)
+  console.log(isLoading)
   return (
     <>
       <h3>{category}</h3>
       <div className="card-grid">
         {images.map((image: any) => (
           <GifItem key={image.id} {...image} />
-          //<li key={id}>{title}</li>
         ))}
       </div>
     </>
---
+++ react-cero-experto-fernando-herrera/04-gif-expert-app/src/hooks/useFetchGifs.ts
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react'
+import { getGifts } from '../helpers/getGifs'
+
+export const useFetchGifs = (category: any) => {
+  const [images, setImages] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const getImages = async () => {
+    const newImages = await getGifts(category)
+    setImages(newImages)
+    setIsLoading(false)
+  }
+  useEffect(() => {
+    getImages()
+  }, [])
+
+  return {
+    images,
+    isLoading,
+  }
+}
