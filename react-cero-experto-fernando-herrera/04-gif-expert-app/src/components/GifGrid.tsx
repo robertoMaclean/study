ce451b4498d32a2c4eb7b99d99a235647963c00b
Roberto Mac lean <roberto.maclean@bicevida.cl>
2023-06-15 21:34:18 -0400

    feat: add class 88 show loading message

--- react-cero-experto-fernando-herrera/04-gif-expert-app/src/components/GifGrid.tsx
+++ react-cero-experto-fernando-herrera/04-gif-expert-app/src/components/GifGrid.tsx
@@ -3,10 +3,10 @@ import { useFetchGifs } from '../hooks/useFetchGifs'
 
 export const GifGrid = ({ category }: any) => {
   const { images, isLoading } = useFetchGifs(category)
-  console.log(isLoading)
   return (
     <>
       <h3>{category}</h3>
+      {isLoading && <h2>Cargando...</h2>}
       <div className="card-grid">
         {images.map((image: any) => (
           <GifItem key={image.id} {...image} />
