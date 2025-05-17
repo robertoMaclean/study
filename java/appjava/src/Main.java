bed4b6bc27d0cc3009cfde735170e90079603330
Roberto Mac lean <roberto.macleanmarinquez@externos-cl.cencosud.com>
2025-05-17 12:15:46 -0400

    feat: Scape characters class 04

--- java/appjava/src/Main.java
+++ java/appjava/src/Main.java
@@ -1,23 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String text = "Hola Mundo!";
-
-        int largo = text.length();
-        System.out.println(largo);
-
-        String texto = text.replace("Hola", "Chao");
-        System.out.println(text);
-
-        System.out.println(texto.endsWith("undo"));
-        System.out.println(texto.startsWith("Ch"));
-        System.out.println(texto.contains("und"));
-        System.out.println(texto.indexOf("Mund"));
-
-        System.out.println(texto.toUpperCase());
-        System.out.println(texto.toLowerCase());
-
-        String malo = "       chanchito feliz      ";
-        System.out.println(malo.trim());
-        System.out.println(malo);
+        String texto = "Hola \tMundo";
+        System.out.println(texto);
     }
 }
