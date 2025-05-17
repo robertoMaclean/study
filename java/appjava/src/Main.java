cd0632017b698816620876af3c00b73679e7c8b7
Roberto Mac lean <roberto.macleanmarinquez@externos-cl.cencosud.com>
2025-05-17 12:25:45 -0400

    feat: Arrays class 05

--- java/appjava/src/Main.java
+++ java/appjava/src/Main.java
@@ -1,6 +1,13 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        String texto = "Hola \tMundo";
-        System.out.println(texto);
+        int[] numeros = { 0,1,2,3,4 };
+        numeros[0] = 5;
+        numeros[4] = 7;
+        System.out.println(Arrays.toString(numeros));
+        System.out.println(numeros.length);
+        Arrays.sort(numeros);
+        System.out.println(Arrays.toString(numeros));
     }
 }
