832df42f8e7f236961bcb730ec4772a87c866d42
Roberto Mac lean <roberto.macleanmarinquez@externos-cl.cencosud.com>
2025-05-17 12:56:14 -0400

    feat: Parse types class 11

--- java/appjava/src/Main.java
+++ java/appjava/src/Main.java
@@ -1,19 +1,20 @@
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
-        /**
-         * PEMDAS
-         * parentesis
-         * potencia
-         * multiplicación
-         * division
-         * suma
-         * resta
-         */
-        int r = (5 + 3) * 2;
+        // Conversión implícita
+        byte a = 1;
+        double b = 15.15;
+        double c = a + b;
+        System.out.println(c);
 
-        System.out.println(r);
+        // Conversión explícita
+        int x = 15;
+        double y = 15.015;
+        int z = x + (int) y;
+        System.out.println(z);
 
+        String j = "1.1";
+        int k = 5;
+        double l = Double.parseDouble(j) + k;
+        System.out.println(l);
     }
 }
