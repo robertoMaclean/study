f5080229d7ee506f72a6cb40160c19a8ddf19179
Roberto Mac lean <roberto.macleanmarinquez@externos-cl.cencosud.com>
2025-05-17 12:43:32 -0400

    feat: Math op class 09

--- java/appjava/src/Main.java
+++ java/appjava/src/Main.java
@@ -2,7 +2,16 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        final String empresa = "Hola Mundo Limited";
-        System.out.println(empresa);
+        int a = 2 + 2;
+        int b = 2 - 2;
+        int c = 3 * 3;
+        int m = 4 % 3;
+        float d = 10f / 3f;
+
+        int x = 5;
+        // int y = x++;
+        x += 5;
+        System.out.println(x);
+        // System.out.println(y);
     }
 }
