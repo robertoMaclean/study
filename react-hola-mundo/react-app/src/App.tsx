4e5ec3778555969128836276a7453d8a569fa897
Roberto Mac lean <roberto.macleanmarinquez@externos-cl.cencosud.com>
2025-05-26 10:47:56 -0400

    Revert "feat: add section Components class 08 print list"
    This reverts commit e5c23613596c8ba9d09ef9ee06ffe12a3ebc0a33.

--- react-hola-mundo/react-app/src/App.tsx
+++ react-hola-mundo/react-app/src/App.tsx
@@ -1,12 +1,9 @@
 import Card, { CardBody } from "./components/Card";
-import List from "./components/List";
 
 function App() {
-  const list = ["Goku", "Tanjiro", "Eren"];
   return (
     <Card>
       <CardBody title="Hola Mundo" text="Este es el texto" />
-      <List data={list} />
     </Card>
   );
 }
