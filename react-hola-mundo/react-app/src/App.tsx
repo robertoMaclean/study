d33ac725b00da45190fb473abf307d9ceea6ff8f
Roberto Mac lean <roberto.macleanmarinquez@externos-cl.cencosud.com>
2025-05-26 10:09:29 -0400

    feat: add section Components class 05 props

--- react-hola-mundo/react-app/src/App.tsx
+++ react-hola-mundo/react-app/src/App.tsx
@@ -1,7 +1,7 @@
 import Card from "./components/Card";
 
 function App() {
-  return <Card />;
+  return <Card body={"Hola Mundo"} />;
 }
 
 export default App;
--- react-hola-mundo/react-app/src/components/Card.tsx
+++ react-hola-mundo/react-app/src/components/Card.tsx
@@ -1,4 +1,9 @@
-function Card() {
+interface Props {
+  body: string;
+}
+
+function Card(props: Props) {
+  const { body } = props;
   return (
     <div
       className="card"
@@ -7,17 +12,22 @@ function Card() {
       }}
     >
       <h5 className="card-header">Featured</h5>
-      <div className="card-body">
-        <h5 className="card-title">Special title treatment</h5>
-        <p className="card-text">
-          With supporting text below as a natural lead-in to additional content.
-        </p>
-        <a href="#" className="btn btn-primary">
-          Go somewhere
-        </a>
-      </div>
+      <div className="card-body">{body}</div>
     </div>
   );
 }
 
+export function CardBody() {
+  return (
+    <>
+      <h5 className="card-title">Special title treatment</h5>
+      <p className="card-text">
+        With supporting text below as a natural lead-in to additional content.
+      </p>
+      <a href="#" className="btn btn-primary">
+        Go somewhere
+      </a>
+    </>
+  );
+}
 export default Card;
