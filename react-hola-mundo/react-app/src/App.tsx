4e6ac6f48a0a9466ebd3f6b6a8ad32b4daac115f
Roberto Mac lean <roberto.macleanmarinquez@externos-cl.cencosud.com>
2025-05-26 11:01:43 -0400

    feat: add section Components class 07 children

--- react-hola-mundo/react-app/src/App.tsx
+++ react-hola-mundo/react-app/src/App.tsx
@@ -1,9 +1,12 @@
 import Card, { CardBody } from "./components/Card";
+import List from "./components/List";
 
 function App() {
+  const list = ["Goku", "Tanjiro", "Eren"];
   return (
     <Card>
       <CardBody title="Hola Mundo" text="Este es el texto" />
+      <List data={list} />
     </Card>
   );
 }
