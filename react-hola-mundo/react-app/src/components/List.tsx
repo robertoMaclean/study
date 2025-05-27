bcc6b44686bb3bf0587c0bd50c502678fda64645
Roberto Mac lean <roberto.macleanmarinquez@externos-cl.cencosud.com>
2025-05-26 20:55:46 -0400

    feat: add section Components class 10 state managment

--- react-hola-mundo/react-app/src/components/List.tsx
+++ react-hola-mundo/react-app/src/components/List.tsx
@@ -1,16 +1,22 @@
+import { useState } from "react";
+
 type Props = {
   data: string[];
 };
 
 function List({ data }: Props) {
-  const handleClick = (e: string) => console.log(e);
+  const [index, setIndex] = useState(1);
+  const handleClick = (i: number) => {
+    setIndex(i);
+  };
+
   return (
     <ul className="list-group">
-      {data.map((elemento) => (
+      {data.map((elemento, i) => (
         <li
-          onClick={() => handleClick(elemento)}
+          onClick={() => handleClick(i)}
           key={elemento}
-          className="list-group-item"
+          className={`list-group-item ${index == i ? "active" : ""}`}
         >
           {elemento}
         </li>
