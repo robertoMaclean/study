83a53b2d1ff137d6c8dc798bdb8a23161767b102
Roberto Mac lean <roberto.macleanmarinquez@externos-cl.cencosud.com>
2025-05-30 18:40:44 -0400

    feat: add section Styles class 02 inline styles

--- react-hola-mundo/react-app/src/App.tsx
+++ react-hola-mundo/react-app/src/App.tsx
@@ -1,23 +1,19 @@
 import { useState } from "react";
 import Button from "./components/Button";
-import Card, { CardBody } from "./components/Card";
+import Card from "./components/Card";
 import List from "./components/List";
 
 function App() {
-  const [isLoadingAdd, setIsLoadingAdd] = useState(false);
-  const [isLoadingRemove, setIsLoadingRemove] = useState(false);
+  // const [isLoadingAdd, setIsLoadingAdd] = useState(false);
+  // const [isLoadingRemove, setIsLoadingRemove] = useState(false);
   const [list, setList] = useState(["Goku", "Tajiro", "Chanchito feliz"]);
   let selectedItem: string;
   const handleClickAdd = () => {
-    // setIsLoadingAdd(!isLoadingAdd);
     setList([...list, "Minion"]);
-    // setIsLoadingAdd(!isLoadingAdd);
   };
   const handleClickRemove = () => {
     console.log("imprimiendo elemento", selectedItem);
-    // setIsLoadingRemove(!isLoadingRemove);
     setList(list.slice(0, -1));
-    // setIsLoadingRemove(!isLoadingRemove);
   };
   const handleSelect = (elemento: string) => {
     console.log("imprimiendo", elemento);
@@ -31,12 +27,8 @@ function App() {
   return (
     <Card>
       {/* <CardBody title="Hola Mundo" text="Este es el texto" /> */}
-      <Button isLoading={isLoadingAdd} onClick={handleClickAdd}>
-        Agregar
-      </Button>
-      <Button isLoading={isLoadingRemove} onClick={handleClickRemove}>
-        Eliminar
-      </Button>
+      <Button onClick={handleClickAdd}>Agregar</Button>
+      <Button onClick={handleClickRemove}>Eliminar</Button>
       {contenido}
       {/* <Button isLoading={isLoading} onClick={handleClick}> */}
       {/*   Hola Mundo */}
