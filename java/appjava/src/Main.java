6151473b75137ba11c82b502eaef77b163be464d
Roberto Mac lean <roberto.macleanmarinquez@externos-cl.cencosud.com>
2025-05-30 22:00:53 -0400

    feat: add section Basic Types class 15 calculator app

--- java/appjava/src/Main.java
+++ java/appjava/src/Main.java
@@ -3,9 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Ingresa tu edad:");
-        byte valor = scanner.nextByte();
-        System.out.println("Tu edad es:");
-        System.out.println(valor);
+        System.out.println("Esta es una aplicación de calculadora");
+        System.out.println("Ingresa el primer número:");
+        int firstValue = scanner.nextInt();
+        System.out.println("Ingresa el segundo número:");
+        int secondValue = scanner.nextInt();
+        System.out.println("La suma es:");
+        System.out.println(firstValue + secondValue);
     }
 }
