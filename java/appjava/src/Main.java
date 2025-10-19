fb34669016952946b53fac08b03fed8eae49c79e
Rob Mac <r.maclean.m@gmail.com>
2025-10-19 19:13:22 -0300

    Java Exceptions class 06

--- java/appjava/src/Main.java
+++ java/appjava/src/Main.java
@@ -4,6 +4,6 @@ import genericos.Utils;
 
 public class Main {
     public static void main(String[] args) {
-        Demo.suma();
+        Demo.comprobado();
     }
 }
--- java/appjava/src/excepciones/Demo.java
+++ java/appjava/src/excepciones/Demo.java
@@ -1,5 +1,9 @@
 package excepciones;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Demo {
     public static void suma() {
         sumando(null);
@@ -7,5 +11,30 @@ public class Demo {
 
     public static Integer sumando(Integer i) {
         return i + 2;
+    }
+
+    public static int noComprobado() {
+        int numerador = 10;
+        int denominador = 0;
+        try {
+            int resultado = numerador / denominador;
+            System.out.println("resultado = " + resultado);
+            return resultado;
+        } catch(RuntimeException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+            return -1;
+        }
+
+    }
+
+    public static void comprobado() {
+        try {
+            FileReader fr = new FileReader("archivo inexistente");
+            var x = 10/0;
+        } catch (FileNotFoundException | ArithmeticException e) {
+            e.printStackTrace();
+            System.out.println("archivo inexistente o dividiste por zero");
+        }
     }
 }
