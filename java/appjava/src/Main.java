05d8ed140d8b9793eaee294f2636b80423ae9509
Rob Mac <r.maclean.m@gmail.com>
2025-10-19 12:01:29 -0300

    Java Exception class 03

--- java/appjava/src/Main.java
+++ java/appjava/src/Main.java
@@ -1,14 +1,9 @@
+import excepciones.Demo;
 import genericos.Garzon;
 import genericos.Utils;
 
 public class Main {
     public static void main(String[] args) {
-        Garzon g1 = new Garzon(1);
-        Garzon g2 = new Garzon(3);
-        if(g1.compareTo(g2) < 0) {
-            System.out.println("g2 es mayor");
-        }
-        var r = Utils.max(1, 15);
-        System.out.println("r = " + r);
+        Demo.suma();
     }
 }
---
+++ java/appjava/src/excepciones/Demo.java
@@ -0,0 +1,11 @@
+package excepciones;
+
+public class Demo {
+    public static void suma() {
+        sumando(null);
+    }
+
+    public static Integer sumando(Integer i) {
+        return i + 2;
+    }
+}
