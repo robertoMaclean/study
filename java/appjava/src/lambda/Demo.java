60ec343ab95b0033bdd805f9549eb2656635f595
Rob Mac <r.maclean.m@gmail.com>
2025-10-21 10:12:35 -0300

    Java lambda expressions class 09 Suppliers

--- java/appjava/src/lambda/Demo.java
+++ java/appjava/src/lambda/Demo.java
@@ -1,17 +1,51 @@
 package lambda;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
 public class Demo {
 
     private static String texto = "Imprimiendo de lambda: ";
-    public static void run() {
+    private static Notificable lambda = message -> System.out.println(texto + message);
 
-        Notificable lambda = message -> {
-            System.out.println(texto + message);
-        };
+    public Demo(String message) {}
+    public static void metodo(String message) {
+
+    }
+
+    public void metodoInstancia(String message) {}
+
+    public static void run() {
         push(lambda);
+        push(message -> metodo(message));
+        push(Demo::metodo);
+        push(System.out::println);
+        Demo demo = new Demo("mi mensaje");
+        push(demo::metodoInstancia);
+        push(Demo::new);
+    }
+
+    public static void incluidas() {
+        List<String> l = List.of("Hola", "Mundo");
+        l.forEach(Demo::sendPush);
+
+        Consumer<String> notify = el -> System.out.println("Enviando a: " + el);
+        Consumer<String> store = el -> System.out.println("Guardando: " + el);
+        l.forEach(notify.andThen(store));
+
+        Supplier<LocalDate> ahora = LocalDate::now;
+
+        System.out.println(ahora.get());
+
     }
 
     public static void push(Notificable n)  {
         n.push("Chanchito feliz");
+    }
+
+    public static void sendPush(String id) {
+        System.out.println("Se ha enviado el id: " + id);
     }
 }
