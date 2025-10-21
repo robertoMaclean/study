88c4e06757b04cb2fbf10ba36d8e0df28e6c4553
Rob Mac <r.maclean.m@gmail.com>
2025-10-20 21:18:51 -0300

    Java lambda expression class 04

--- java/appjava/src/lambda/Demo.java
+++ java/appjava/src/lambda/Demo.java
@@ -2,7 +2,13 @@ package lambda;
 
 public class Demo {
     public static void run() {
-        push(new Notification());
+        push(new Notificable() {
+            @Override
+            public void push(String message) {
+                System.out.println("Enviado desde clas anonima: " + message);
+            }
+        });
+        push((message) -> System.out.println("Imprimiendo de lambda: " + message));
     }
 
     public static void push(Notificable n)  {
